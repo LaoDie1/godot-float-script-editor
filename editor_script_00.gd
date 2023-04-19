c18998992d662279c5d2be48c00aeee60cc9e78c
LaoDie1 <1693619798@qq.com>
2023-04-19 19:00:22 +0800

    first commit

---
+++ editor_script_00.gd
@@ -0,0 +1,19 @@
+# editor_script_00.gd
+@tool
+extends EditorScript
+
+
+var script_editor = get_editor_interface().get_script_editor()
+
+
+func _run():
+	pass
+	
+	print(script_editor.get_child(0))
+	# sadsas
+	# sadsasasdassda
+	# sadsas
+	# sadsas
+	# sadsas
+	
+
---
+++ plugin.gd
@@ -0,0 +1,173 @@
+#============================================================
+#    Plugin
+#============================================================
+# - author: zhangxuetu
+# - datetime: 2023-04-19 13:14:14
+# - version: 4.0
+#============================================================
+@tool
+extends EditorPlugin
+
+
+var script_editor : ScriptEditor
+var script_sub_container : Control
+
+var dialog : Window = Window.new()
+var float_button : Button = Button.new()
+
+var last_main_screen_name: String
+var enable_main_changed: bool = true
+
+
+#============================================================
+#  内置
+#============================================================
+func _enter_tree():
+	# 代码辑器
+	script_editor = get_editor_interface().get_script_editor()
+	# 代码编辑器子节点容器
+	script_sub_container = script_editor.get_child(0)
+	
+	# 当前屏幕视图
+	last_main_screen_name = get_current_screen()
+	
+	# 浮动窗口
+	dialog.title = "Script Editor Dialog"
+	dialog.size = Vector2(500, 350)
+	dialog.wrap_controls = true
+	dialog.visible = false
+	dialog.handle_input_locally = false
+	dialog.gui_embed_subwindows = true
+	script_editor.add_child(dialog)
+	dialog.close_requested.connect(func(): float_button.button_pressed = false )
+	
+	# 容器节点
+	var container : MarginContainer = MarginContainer.new()
+	var panel : Panel = Panel.new()
+	container.add_child(panel)
+	dialog.add_child(container)
+	panel.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	panel.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	container.set_anchors_preset(Control.PRESET_FULL_RECT)
+	container.size = dialog.size
+	dialog.size_changed.connect(func(): 
+		container.size = dialog.size
+	)
+	
+	# 添加浮动菜单按钮
+	var menu_container = script_sub_container.get_child(0)
+	menu_container.add_child(float_button)
+	float_button.text = "Float"
+	float_button.toggle_mode = true
+	float_button.toggled.connect(func(button_pressed: bool):
+		if button_pressed:
+			# 浮动
+			script_editor.remove_child(script_sub_container)
+			container.add_child(script_sub_container)
+			
+			await Engine.get_main_loop().process_frame
+			_change_to_last_screen()
+			dialog.popup_centered()
+		
+		else:
+			# 取消浮动
+			if script_sub_container != script_editor.get_child(0):
+				script_sub_container \
+					.get_parent() \
+					.remove_child(script_sub_container)
+				script_editor.add_child(script_sub_container, true)
+				script_editor.move_child(script_sub_container, 0)
+			if dialog.visible:
+				dialog.hide()
+			
+			if dialog.visible:
+				dialog.hide()
+			get_editor_interface().set_main_screen_editor("Script")
+		
+	)
+	
+	self.main_screen_changed.connect(_main_changed)
+	
+	
+	dialog.window_input.connect(func(event):
+		if event is InputEventKey:
+			if event.pressed and event.ctrl_pressed:
+				if event.keycode == KEY_S:
+					Engine.get_main_loop().root.push_unhandled_input(event, true)
+					dialog.move_to_foreground()
+		
+	)
+	
+	
+
+
+func _exit_tree():
+	float_button.button_pressed = false
+	main_screen_changed.disconnect(_main_changed)
+	
+	dialog.queue_free()
+	float_button.queue_free()
+
+
+func _handles(object):
+	if float_button.button_pressed and object is GDScript:
+		_popup()
+
+
+#============================================================
+#  自定义
+#============================================================
+func _popup():
+	dialog.popup()
+	dialog.gui_release_focus()
+	dialog.move_to_foreground()
+	if get_current_screen() == "Script":
+		_change_to_last_screen()
+
+
+func _change_to_last_screen():
+	get_editor_interface().set_main_screen_editor("Script")
+	if last_main_screen_name == "Script": 
+		last_main_screen_name = "2D"
+	get_editor_interface().set_main_screen_editor(last_main_screen_name)
+
+
+func get_current_screen_node() -> Control:
+	for child in get_editor_interface() \
+		.get_editor_main_screen() \
+		.get_children():
+		if child is Control and child.visible:
+			return child
+	return null
+
+func get_current_screen() -> String:
+	var child = get_current_screen_node()
+	if child is Control and child.visible:
+		match child.get_class():
+			"CanvasItemEditor":
+				return "2D"
+			"Node3DEditor":
+				return "3D"
+			"ScriptEditor":
+				return "Script"
+			"EditorAssetsLibrary":
+				return "AssetLib"
+	# default 2D viewport
+	return "2D"
+
+
+#============================================================
+#  连接信号
+#============================================================
+func _main_changed(screen_name: String):
+	if enable_main_changed:
+		if float_button.button_pressed and screen_name == "Script":
+			_popup()
+			
+			# 如果当前是 Script 视图，则切换到 Script 以外的视图中
+			if get_current_screen() == "Script":
+				_change_to_last_screen()
+		
+		else:
+			last_main_screen_name = screen_name
+
